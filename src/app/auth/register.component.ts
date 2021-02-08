f60a078ef1108bda8be3a7ff4a591ef16c0aa086
Maxim Tugay <xmax13@gmail.com>
2021-02-08 23:24:35 +0300

    Register page

--- src/app/auth/register.component.ts
+++ src/app/auth/register.component.ts
@@ -1,15 +1,65 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { Validators, FormBuilder } from '@angular/forms';
+import { UserLogin } from '../model/user.model';
 
 @Component({
-    templateUrl: 'register.component.html'
+    templateUrl: 'register.component.html',
+    styleUrls: ['./login.component.scss']
 })
-
 export class RegisterComponent {
-    constructor(private auth: AuthService, private router: Router) { }
+    inProgress: boolean = false;
+    errorMessage: string = '';
+    hidePassword1: boolean = true;
+    hidePassword2: boolean = true;
+    agreementChecked: boolean = false;
+
+    signupForm = this.formBuilder.group({
+        email: [, 
+            { validators: [
+                Validators.required,
+                Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+            ], updateOn: "change" }
+        ],
+        username: [, 
+            { validators: [Validators.required], updateOn: "change" }
+        ],
+        userType: ['User'],
+        password1: [, 
+            { validators: [
+                Validators.required, 
+                Validators.minLength(8)
+            ], updateOn: "change" }
+        ],
+        password2: [, 
+            { validators: [
+                Validators.required, 
+                Validators.minLength(8)
+            ], updateOn: "change" }
+        ]
+    });
+
+    constructor(private auth: AuthService, private formBuilder: FormBuilder, private router: Router) { }
     
-    isAuthenticated(): boolean {
-        return this.auth.authenticated;
-    }    
+    checkAgreement() {
+        this.agreementChecked = !this.agreementChecked;
+    }
+
+    passwordsEqual(): boolean {
+        let p1 = this.signupForm.get('password1')?.value;
+        let p2 = this.signupForm.get('password2')?.value;
+        return (p1 == p2);
+    }
+
+    onSignupSubmit() {
+        this.errorMessage = '';
+        if (this.signupForm.valid) {
+            if (!this.passwordsEqual()) {
+                this.errorMessage = 'Passwords are not equal';
+                return;
+            }
+            this.inProgress = true;
+        }
+    }
 }
