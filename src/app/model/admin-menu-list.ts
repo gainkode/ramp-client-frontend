d79517b195feaf7df0820d1e5013678e163cd050
Maxim Tugay <xmax13@gmail.com>
2021-03-25 00:21:27 +0300

    Admin page styling

--- src/app/model/admin-menu-list.ts
+++ src/app/model/admin-menu-list.ts
@@ -2,62 +2,74 @@ export interface AdminMenuItem {
     id: string;
     name: string;
     url: string;
+    icon: string;
 }
 
 export const AdminMenuItems: Array<AdminMenuItem> = [
     {
         id: 'dashboard',
         name: 'Dashboard',
-        url: '/admin/main/dashboard'
+        url: '/admin/main/dashboard',
+        icon: 'dashboard'
     },
     {
         id: 'transactions',
         name: 'Transactions',
-        url: '/admin/main/transactions'
+        url: '/admin/main/transactions',
+        icon: 'compare_arrows'
     },
     {
         id: 'customers',
         name: 'Customers',
-        url: '/admin/main/customers'
+        url: '/admin/main/customers',
+        icon: 'shopping_bag'
     },
     {
         id: 'wallets',
         name: 'Wallets',
-        url: '/admin/main/wallets'
+        url: '/admin/main/wallets',
+        icon: 'wallet_membership'
     },
     {
         id: 'fees',
         name: 'Fees',
-        url: '/admin/main/fees'
+        url: '/admin/main/fees',
+        icon: 'payment'
     },
     {
         id: 'notifications',
         name: 'Notifications',
-        url: '/admin/main/notifications'
+        url: '/admin/main/notifications',
+        icon: 'notifications'
     },
     {
         id: 'costs',
         name: 'Costs',
-        url: '/admin/main/costs'
+        url: '/admin/main/costs',
+        icon: 'attach_money'
     },
     {
         id: 'reconciliation',
         name: 'Reconciliation',
-        url: '/admin/main/reconciliation'
+        url: '/admin/main/reconciliation',
+        icon: 'cloud_off'
     },
     {
         id: 'system-users',
         name: 'System Users',
-        url: '/admin/main/system-users'
+        url: '/admin/main/system-users',
+        icon: 'account_box'
     },
     {
         id: 'risk-center',
         name: 'Resk Center',
-        url: '/admin/main/risk-center'
+        url: '/admin/main/risk-center',
+        icon: 'bolt'
     },
     {
         id: 'affiliates',
         name: 'Affiliates',
-        url: '/admin/main/affiliates'
+        url: '/admin/main/affiliates',
+        icon: 'device_hub'
     }
 ];
