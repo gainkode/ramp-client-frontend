1be09a379be282d852b867beb4b166e07062bb2f
Maxim Tugay <xmax13@gmail.com>
2021-04-06 22:10:08 +0300

    Checkout wizard template

--- src/app/quick-checkout/quick-checkout.component.ts
+++ src/app/quick-checkout/quick-checkout.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { User } from '../model/generated-models';
 import { QuickCheckoutDataService } from '../services/quick-checkout.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: 'quick-checkout.component.html',
     styleUrls: ['quick-checkout.scss']
 })
-export class QuuckCheckoutComponent {
+export class QuuckCheckoutComponent implements OnInit {
     user: User | null = null;
+    firstFormGroup!: FormGroup;
+    secondFormGroup!: FormGroup;
 
     constructor(private auth: AuthService, private dataService: QuickCheckoutDataService,
-        private router: Router) {
+        private formBuilder: FormBuilder, private router: Router) {
         this.user = auth.user;
+    }
+
+    ngOnInit(): void {
+        this.firstFormGroup = this.formBuilder.group({
+            firstCtrl: ['', Validators.required]
+        });
+        this.secondFormGroup = this.formBuilder.group({
+            secondCtrl: ['', Validators.required]
+        });
     }
 }
--- src/app/quick-checkout/quick-checkout.module.ts
+++ src/app/quick-checkout/quick-checkout.module.ts
@@ -14,6 +14,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatStepperModule } from '@angular/material/stepper';
 import { ComponentsModule } from '../components/components.module';
 import { QuuckCheckoutComponent } from './quick-checkout.component';
 
@@ -38,6 +39,7 @@ const modules = [
     MatAutocompleteModule,
     MatIconModule,
     MatProgressBarModule,
+    MatStepperModule,
     ComponentsModule
 ];
 
