9f829cf2fbce97e98cdd0473482be0f6f809e6c8
Maxim Tugay <xmax13@gmail.com>
2021-05-27 21:18:34 +0300

    Get ready to tests

--- src/app/quick-checkout/quick-checkout.component.ts
+++ src/app/quick-checkout/quick-checkout.component.ts
@@ -718,3 +718,67 @@ export class QuickCheckoutComponent implements OnInit, OnDestroy {
         return error;
     }
 }
+
+
+
+
+
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta http-equiv="content-type" content="text/html; charset=UTF-8" >     
+//     <meta http-equiv="refresh" content="10;URL='https://channel.paragon.online/payment-form/logs/e8M9iKQA'" />    
+
+//     <title>Redirecting ...</title>
+// </head>
+// <body>
+//     Redirecting...
+//     <form name="returnform" action="https://channel.paragon.online/payment-redirect/e8M9iKQA" method="get">
+        
+//         <noscript>
+//                 <input type="submit" name="submit" value="Press this button to continue"/>
+//         </noscript>
+//     </form>
+
+//     <script>
+        
+//     function run(){
+//             try {
+
+//             var a = window.screen.height,
+//             r = window.screen.width,
+//             s = window.screen.colorDepth,
+//             i = navigator.javaEnabled(),
+//             o = navigator.userAgent,
+//             c = new Date().getTimezoneOffset(),
+//             d = (navigator.browserLanguage !== undefined) ? navigator.browserLanguage : navigator.language;
+//             var fingerprintUrl = "".concat("https://channel.paragon.online/", "fingerprint/").concat("e8M9iKQA");
+
+//             var fingerprintClient = new XMLHttpRequest();
+//             fingerprintClient.open("POST", fingerprintUrl, true);
+
+//             fingerprintClient.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+//             fingerprintClient.onreadystatechange = function() { 
+//                 if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+//                     console.log("submit form");
+//                     document.returnform.submit();
+//                 }
+//             };
+
+//         fingerprintClient.send("time_zone_offset=" + c + "&screen_height=" + a + "&screen_width=" + r + "&color_depth=" + s + "&java_enabled=" + i + "&user_agent=" + o + "&language=" + d);
+//     } catch (ex) {
+//         console.log("There was an error in code");
+//         var xhr = new XMLHttpRequest();
+//         xhr.open("POST", "https://channel.paragon.online/" + "payment-form/logs");
+//         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//         xhr.send("Error=" + ex.message + "&SerialNumber=".concat("e8M9iKQA")); 
+//     }
+// }
+// run();
+//     </script>
+// </body>
+// </html>
