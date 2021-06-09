785638e98dd6f951826560c437140937df348846
Maxim Tugay <xmax13@gmail.com>
2021-06-10 00:20:36 +0300

    New personal main page layout - menu bar

--- src/app/personal/personal.component.ts
+++ src/app/personal/personal.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CardView } from '../model/payment.model';
+import { UserType } from '../model/generated-models';
 import { AuthService } from '../services/auth.service';
 import { NotificationService } from '../services/notification.service';
 
 @Component({
-    templateUrl: 'personal.component.html'
+    templateUrl: 'personal.component.html',
+    styleUrls: ['../menu.scss']
 })
 export class PersonalComponent implements OnInit {
     constructor(private auth: AuthService, private notification: NotificationService,
         private router: Router) { }
+
+    get userName(): string {
+        let name = '';
+        const user = this.auth.user;
+        if (user) {
+            name = `${user.firstName} ${user.lastName}`;
+        }
+        return name;
+    }
 
     ngOnInit(): void {
         this.notification.subscribeToNotifications().subscribe(({ data }) => {
@@ -17,6 +27,10 @@ export class PersonalComponent implements OnInit {
         }, (error) => {
             // there was an error subscribing to notifications
         });
+    }
+
+    routeTo(link: string): void {
+        this.router.navigateByUrl(link);
     }
 
     logout(): void {
