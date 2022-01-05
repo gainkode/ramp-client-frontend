eda3fb81b349b2d0ca1aef36f945684ac92097b0
xmax <xmax13@gmail.com>
2022-01-06 00:33:38 +0300

    Privacy policy layout complete

--- src/app/terms.component.ts
+++ src/app/terms.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-    templateUrl: 'terms.component.html'
+    templateUrl: 'terms.component.html',
+    styleUrls: ['../assets/button.scss', '../assets/intro.scss']
 })
 export class TermsComponent {
     constructor(private router: Router) { }
+
+    routeTo(link: string): void {
+        this.router.navigateByUrl(link);
+    }
 }
