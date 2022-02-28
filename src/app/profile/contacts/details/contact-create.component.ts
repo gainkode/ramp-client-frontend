126766b5ba59714061255715eb90a35a2683ade1
xmax <xmax13@gmail.com>
2022-02-28 11:15:04 +0300

    Contact form email validator

--- src/app/profile/contacts/details/contact-create.component.ts
+++ src/app/profile/contacts/details/contact-create.component.ts
@@ -30,7 +30,7 @@ export class ProfileContactCreateComponent implements OnInit, OnDestroy {
             {
                 validators: [
                     Validators.required,
-                    Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+                    Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
                 ],
                 updateOn: 'change',
             }
