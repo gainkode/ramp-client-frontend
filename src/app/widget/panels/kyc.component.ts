755799b56070621403e81c0169965f53ec54f7ce
paulgraz <pavel.grazhdankin@gmail.com>
2022-12-07 19:17:32 +0100

    Revert "Revert "shuftiNotificationFix""
    This reverts commit e0c46fb6c879db0766b9e2f622b3856d91c52730.

--- src/app/widget/panels/kyc.component.ts
+++ src/app/widget/panels/kyc.component.ts
@@ -19,6 +19,7 @@ export class WidgetKycComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() completedWhenVerified = false;
   @Input() allowToPayIfKycFailed = false;
   @Input() summary: CheckoutSummary | undefined = undefined;
+  @Input() shuftiSubscribeResult: boolean | undefined = undefined;
   @Output() onError = new EventEmitter<string>();
   @Output() onReject = new EventEmitter();
   @Output() onAuthError = new EventEmitter();
@@ -53,7 +54,13 @@ export class WidgetKycComponent implements OnInit, OnDestroy, AfterViewInit {
   ngOnDestroy(): void {
     this.pSubscriptions.unsubscribe();
   }
-
+  ngOnChanges(): void {
+    if(this.shuftiSubscribeResult == false){
+      this.handleReject();
+    }else if(this.shuftiSubscribeResult == true){
+      this.onNext.emit();
+    }
+  }
   handleReject(): void {
     this.rejectKyc = true;
   }
