3d0d7ece904c1aad051b06a2a1fef1816d278d90
Dinobugain <85806848+Dinobugain@users.noreply.github.com>
2023-03-24 16:26:39 +0700

    test environments

---
+++ src/environments/testnet/custom_data.sql
@@ -0,0 +1,26 @@
+UPDATE ewallet.t_settings_common
+SET text_pages='{
+    "{\"page\":1,\"text\":\"[%title%]DISCLAIMER\"}",
+    "{\"page\":1,\"text\":\"[%paragraph%]\"}",
+    "{\"page\":1,\"text\":\"Welcome to {%product%}!\"}",
+    "{\"page\":1,\"text\":\"[%paragraph%]\"}",
+    "{\"page\":1,\"text\":\"Dear Customer,\"}",
+    "{\"page\":1,\"text\":\"Please note that you are about to transfer funds using {%product%} services.\"}",
+    "{\"page\":1,\"text\":\"{%product%} is a separate company that provides money transfer services from AUD to Crypto.\"}",
+    "{\"page\":1,\"text\":\"[%paragraph%]\"}",
+    "{\"page\":1,\"text\":\"Risk Disclaimer:\"}",
+    "{\"page\":1,\"text\":\"Due to the nature of Crypto-currencies transactions, once a payment is done and your order has been paid, we will not be able to reverse or reimburse your payment.\"}",
+    "{\"page\":1,\"text\":\"[%paragraph%]\"}",
+    "{\"page\":1,\"text\":\"Before using our services we do recommend you to read our <%terms%>Terms and Conditions and Cookies</%terms%> Policy\"}",
+    "{\"page\":1,\"text\":\"[%paragraph%]\"}",
+    "{\"page\":1,\"text\":\"[%accept%]I have read and accept the terms and conditions, cookies policy and disclaimer\"}",
+    "{\"page\":2,\"text\":\"[%main_title%]ORDER COMPLETED\"}",
+    "{\"page\":2,\"text\":\"Dear Customer,\"}",
+    "{\"page\":2,\"text\":\"Thank you for choosing {%product%}.\"}",
+    "{\"page\":2,\"text\":\"[%paragraph%]\"}",
+    "{\"page\":2,\"text\":\"Your order has been successfully received,\"}",
+    "{\"page\":2,\"text\":\"As soon as your payment arrives from your bank we will instantly transfer it to the merchant account.\"}",
+    "{\"page\":2,\"text\":\"[%paragraph%]\"}",
+    "{\"page\":2,\"text\":\"Should you have any further inquiries please contact us: {%support%}.\"}"
+}'
+
