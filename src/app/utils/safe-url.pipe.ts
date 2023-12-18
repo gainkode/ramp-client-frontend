8fc5acfedf022786cbee5d943b04b9229ba262fc
Diizer <diizer_d@ukr.net>
2023-12-18 14:35:11 +0200

    fix img_

--- src/app/utils/safe-url.pipe.ts
+++ src/app/utils/safe-url.pipe.ts
@@ -1,11 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Pipe({ name: 'safeurl' })
 export class SafeUrlPipe implements PipeTransform {
 	constructor(private domSanitizer: DomSanitizer) { }
-	transform(url: string): SafeResourceUrl {
-		//return this.domSanitizer.sanitize(SecurityContext.URL, url);
+	transform(url: string, isImage: boolean = false): SafeResourceUrl | SafeUrl {
+		if (isImage) {
+			return this.domSanitizer.bypassSecurityTrustUrl(url);
+		}
+
 		return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
 	}
 }
