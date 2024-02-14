2108fc33fed148feac0de843ef0e6f281e589732
Diizer <diizer_d@ukr.net>
2024-02-14 17:36:39 +0200

    improve fee panel

--- src/app/admin/components/fees/details/fee-similiar-panel/fee-similiar-panel.component.ts
+++ src/app/admin/components/fees/details/fee-similiar-panel/fee-similiar-panel.component.ts
@@ -1,20 +1,16 @@
 import { NgFor, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { SettingsFeeSimilarObject } from 'model/generated-models';
 
 @Component({
-  standalone: true,
-  selector: 'app-fee-similiar-panel',
-  templateUrl: './fee-similiar-panel.component.html',
-  styleUrls: ['./fee-similiar-panel.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatExpansionModule, NgFor, NgIf],
+	standalone: true,
+	selector: 'app-fee-similiar-panel',
+	templateUrl: './fee-similiar-panel.component.html',
+	styleUrls: ['./fee-similiar-panel.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [MatExpansionModule, NgFor, NgIf],
 })
-export class FeeSimiliarPanelComponent implements OnInit {
-  targetField: any;
-
-
-  ngOnInit(): void {
-      console.log(this.targetField)
-  }
+export class FeeSimiliarPanelComponent {
+  @Input() targetField: SettingsFeeSimilarObject;
 }
